c1189ff6b36c5d9241cd930eccfbfebc0aa07482
Jericho Aquino <yeoligoakino@gmail.com>
2020-12-30 18:02:29 +0800

    Deploy

---
+++ docs/precache-manifest.ed983fbcb3822e513d213b9c2fefcf4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b14d68db433104060a",
+    "url": "/portfolio/css/app.4d1f97cb.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/portfolio/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c2f847bbc12e2398e68d1ca3578a4bf4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e13be4d293b455d1f64f",
+    "url": "/portfolio/js/about.bf814a3f.js"
+  },
+  {
+    "revision": "62b14d68db433104060a",
+    "url": "/portfolio/js/app.80bbd286.js"
+  },
+  {
+    "revision": "942a841bdc26234fe999",
+    "url": "/portfolio/js/chunk-vendors.8380736d.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/portfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio/robots.txt"
+  }
+]);
